3c15c531d88a52fbfe1cae32365d6d443b4f8b03
snowhsw <snowhswasd@naver.com>
2025-08-06 00:00:20 +0900

    modified:   typescriptStudy/src/test/test07.tsx

--- typescriptStudy/src/test/test07.tsx
+++ typescriptStudy/src/test/test07.tsx
@@ -9,12 +9,29 @@ const Test07 = () =>{
     let myInfo : "남자"|"27살";
     myInfo = "남자"
 
+    //파라미터와 리턴값에도 적용 가능
     const testFn1 = ( a: "hello") : 1|0 =>{
-
         return 0
     }
+    const testFn2 = (params: "가위"|"바위"|"보") : ("가위"|"바위"|"보")[] =>{
 
-    const testFn2
+        return ["가위","바위"]
+    }
+    testFn2("가위")
+
+    //as const는 object의 value의 값을 그대로 타입으로 지정해준다.
+    //as const는 object의 속성들에 모두 readonly로 바꿔준다.
+    var 자료 ={
+        name: "kim"
+    } as const;
+
+    //내용이 kim이 아니라 타입이 kim이어야 함
+    const testFn3 = (a: "kim") =>{
+
+    }
+    
+    //자료.name은 값이 Kim이지 타입은 string이다
+    testFn3(자료.name)
     return null;
 }
 
