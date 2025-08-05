e574b79cca3542225f10f52a71deb1aa9a5b6722
snowhsw <snowhswasd@naver.com>
2025-08-05 23:49:08 +0900

    new file:   typescriptStudy/src/test/test07.tsx

---
+++ typescriptStudy/src/test/test07.tsx
@@ -0,0 +1,21 @@
+//Literal Types로 만드는 const 변수 유사품
+
+const Test07 = () =>{
+
+    //좀 더 엄격한 타입 지정
+    let 이름 : "kim" ;
+
+    //유니온 타입도 가능
+    let myInfo : "남자"|"27살";
+    myInfo = "남자"
+
+    const testFn1 = ( a: "hello") : 1|0 =>{
+
+        return 0
+    }
+
+    const testFn2
+    return null;
+}
+
+export default Test07;
